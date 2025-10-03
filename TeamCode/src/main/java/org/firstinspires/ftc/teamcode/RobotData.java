b195b22c2f36f7e24b12231d7bce32b5cd1cfa51
Smalley <ksmalley@harrisonschools.com>
2025-10-03 19:25:11 -0400

    feat: Update Gradle and add RobotData class
    Updates Gradle wrapper to 8.13 and Android Gradle Plugin to 8.13.0.
    Introduces `RobotData.java` to persist robot state between OpModes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class RobotData {
+    // Static variables persist between OpModes
+    public static double autoEndX = 0;
+    public static double autoEndY = 0;
+    public static double autoEndHeading = 0;
+    public static boolean autoCompleted = false;
+
+    // You can also store more complex data
+    public static String selectedStrategy = "default";
+}
